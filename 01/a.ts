a24b60b377f10b689ab1b2545a3aa788a26eb053
Sven <svenrisse0@gmail.com>
2024-01-06 20:45:33 +0100

    01/a solved

--- 01/a.ts
+++ 01/a.ts
@@ -1,3 +1,19 @@
 const input = await Bun.file("./input.txt").text();
 
-console.log(input);
+const split = input.split("\n\n");
+
+let max = 0;
+
+split.map((elf) => {
+  let temp = 0;
+  const calories = elf.split("\n");
+  calories.map((row) => {
+    temp += parseInt(row);
+  });
+
+  if (temp > max) {
+    max = temp;
+  }
+});
+
+console.log(max);
